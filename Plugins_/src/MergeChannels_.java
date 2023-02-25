70549e86a003fc0355be1a33e7e748baf4c91b24
Weriks Santos <werikscs@gmail.com>
2023-02-25 14:57:12 -0300

    refactor plugin merge channels

--- Plugins_/src/MergeChannels_.java
+++ Plugins_/src/MergeChannels_.java
@@ -13,7 +13,7 @@ public class MergeChannels_ implements PlugIn {
 		// pegar os titulos das imagens abertas
 		String imgTitles[] = WindowManager.getImageTitles();
 
-		// pegar as imagens que tenham no titulo a identificação "splitted"
+		// pegar as imagens que tenham no titulo a sequencia "splitted"
 		String SEQUENCE = "splitted";
 		ArrayList<ImagePlus> splitedImgsArray = new ArrayList<ImagePlus>();
 		ImagePlus referenceImg = null;
@@ -33,33 +33,38 @@ public class MergeChannels_ implements PlugIn {
 		ImageProcessor imgProcessor = mergedImg.getProcessor();
 
 		// inserir os pixels das imagens na imagens final
-		int count = 0;
+		int channel = 0;
 		for (ImagePlus splitedImg : splitedImgsArray) {
 			ImageProcessor splitedImgProcessor = splitedImg.getProcessor();
+			
 			for (int row = 0; row < imgHeight; row++) {
 				for (int column = 0; column < imgWidth; column++) {
-					int pixelValueSplitedImg[] = splitedImgProcessor.getPixel(column, row, null);
+					int pixelValueSplitedImg = splitedImgProcessor.getPixel(column, row, null)[0];
 					int pixelValueMergedImg[] = mergedImg.getPixel(column, row);
-					
-					if(count == 0) {
-						int a[] = {pixelValueSplitedImg[0],pixelValueMergedImg[1],pixelValueMergedImg[2]};
-						imgProcessor.putPixel(column, row, a);
+					int RChannel = 0, GChannel = 0, BChannel = 0;
+
+					if (channel == 0) {
+						RChannel = pixelValueSplitedImg;
+						GChannel = pixelValueMergedImg[1];
+						BChannel = pixelValueMergedImg[2];
+					}
+
+					if (channel == 1) {
+						RChannel = pixelValueMergedImg[0];
+						GChannel = pixelValueSplitedImg;
+						BChannel = pixelValueMergedImg[2];
+					}
+
+					if (channel == 2) {
+						RChannel = pixelValueMergedImg[0];
+						GChannel = pixelValueMergedImg[1];
+						BChannel = pixelValueSplitedImg;
 					}
 					
-					if(count == 1) {
-						int a[] = {pixelValueMergedImg[0],pixelValueSplitedImg[0],pixelValueMergedImg[2]};
-						imgProcessor.putPixel(column, row, a);
-					}
-					
-					if(count == 2) {
-						int a[] = {pixelValueMergedImg[0],pixelValueMergedImg[1],pixelValueSplitedImg[0]};
-						imgProcessor.putPixel(column, row, a);
-					}
-					
-					
+					imgProcessor.putPixel(column, row, new int[]{ RChannel, GChannel, BChannel });
 				}
 			}
-			count++;
+			channel++;
 		}
 
 		mergedImg.show();
