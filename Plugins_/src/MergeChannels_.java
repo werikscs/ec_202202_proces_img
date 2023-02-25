51ed4fd8e3e9b46db4d6ca701c68f1eec489aaad
Weriks Santos <werikscs@gmail.com>
2023-02-25 18:02:40 -0300

    refactor merge channels plugin

--- Plugins_/src/MergeChannels_.java
+++ Plugins_/src/MergeChannels_.java
@@ -10,65 +10,77 @@ public class MergeChannels_ implements PlugIn {
 
 	@Override
 	public void run(String arg) {
-		// pegar os titulos das imagens abertas
-		String imgTitles[] = WindowManager.getImageTitles();
 
-		// pegar as imagens que tenham no titulo a sequencia "splitted"
-		String SEQUENCE = "splitted";
+		String text = "splitted";
+		ArrayList<ImagePlus> splitedImgsArray = getOpenedImagesByTextInTitle(text);
+		ImagePlus referenceImg = splitedImgsArray.get(0);
+		ImagePlus mergedImg = createRGBImage(referenceImg, splitedImgsArray);
+
+		mergedImg.show();
+	}
+	
+	private ArrayList<ImagePlus> getOpenedImagesByTextInTitle(String text) {
+		String imgTitles[] = WindowManager.getImageTitles();
 		ArrayList<ImagePlus> splitedImgsArray = new ArrayList<ImagePlus>();
-		ImagePlus referenceImg = null;
 
 		for (String title : imgTitles) {
-			if (title.contains(SEQUENCE)) {
+			if (title.contains(text)) {
 				ImagePlus tempImg = WindowManager.getImage(title);
 				splitedImgsArray.add(tempImg);
-				referenceImg = tempImg;
 			}
 		}
+		return splitedImgsArray;
+	}
 
-		// criar a imagem que será um merge das splitted
+	private ImagePlus createRGBImage(ImagePlus referenceImg, ArrayList<ImagePlus> splitedImgsArray) {
 		int imgWidth = referenceImg.getWidth();
 		int imgHeight = referenceImg.getHeight();
 		ImagePlus mergedImg = IJ.createImage("merged", "RGB black", imgWidth, imgHeight, 1);
 		ImageProcessor imgProcessor = mergedImg.getProcessor();
-
-		// inserir os pixels das imagens na imagens final
 		int channel = 0;
+
 		for (ImagePlus splitedImg : splitedImgsArray) {
 			ImageProcessor splitedImgProcessor = splitedImg.getProcessor();
-			
+
 			for (int row = 0; row < imgHeight; row++) {
 				for (int column = 0; column < imgWidth; column++) {
 					int pixelValueSplitedImg = splitedImgProcessor.getPixel(column, row, null)[0];
 					int pixelValueMergedImg[] = mergedImg.getPixel(column, row);
-					int RChannel = 0, GChannel = 0, BChannel = 0;
 
-					if (channel == 0) {
-						RChannel = pixelValueSplitedImg;
-						GChannel = pixelValueMergedImg[1];
-						BChannel = pixelValueMergedImg[2];
-					}
+					int RGBPixelValue[] = createRGBPixel(channel, pixelValueSplitedImg, pixelValueMergedImg);
 
-					if (channel == 1) {
-						RChannel = pixelValueMergedImg[0];
-						GChannel = pixelValueSplitedImg;
-						BChannel = pixelValueMergedImg[2];
-					}
-
-					if (channel == 2) {
-						RChannel = pixelValueMergedImg[0];
-						GChannel = pixelValueMergedImg[1];
-						BChannel = pixelValueSplitedImg;
-					}
-					
-					imgProcessor.putPixel(column, row, new int[]{ RChannel, GChannel, BChannel });
+					imgProcessor.putPixel(column, row, RGBPixelValue);
 				}
 			}
 			splitedImg.close();
 			channel++;
 		}
 
-		mergedImg.show();
+		return mergedImg;
+	}
+
+	private int[] createRGBPixel(int channel, int pixelValueSplitedImg, int[] pixelValueMergedImg) {
+		int RChannel = 0, GChannel = 0, BChannel = 0;
+		
+		if (channel == 0) {
+			RChannel = pixelValueSplitedImg;
+			GChannel = pixelValueMergedImg[1];
+			BChannel = pixelValueMergedImg[2];
+		}
+
+		if (channel == 1) {
+			RChannel = pixelValueMergedImg[0];
+			GChannel = pixelValueSplitedImg;
+			BChannel = pixelValueMergedImg[2];
+		}
+
+		if (channel == 2) {
+			RChannel = pixelValueMergedImg[0];
+			GChannel = pixelValueMergedImg[1];
+			BChannel = pixelValueSplitedImg;
+		}
+		
+		return new int[] {RChannel, GChannel, BChannel};
 	}
 
 }
