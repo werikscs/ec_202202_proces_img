e28d6235c5493abd22ebc83c790c9cc232b3d37c
Weriks Santos <werikscs@gmail.com>
2023-02-25 11:26:44 -0300

    create split images from rgb to grayscale

---
+++ Plugins_/src/SplitChannels_.java
@@ -0,0 +1,36 @@
+import ij.IJ;
+import ij.ImagePlus;
+import ij.plugin.PlugIn;
+import ij.process.ImageProcessor;
+
+public class SplitChannels_ implements PlugIn {
+
+	@Override
+	public void run(String arg) {
+		ImagePlus img = IJ.getImage();
+		int imgWidth = img.getWidth();
+		int imgHeight = img.getHeight();
+		String imgTitle = img.getShortTitle();
+
+		splitImageToChannel(imgTitle + " - " + "R", 0, imgWidth, imgHeight, img);
+		splitImageToChannel(imgTitle + " - " + "G", 1, imgWidth, imgHeight, img);
+		splitImageToChannel(imgTitle + " - " + "B", 2, imgWidth, imgHeight, img);
+	}
+
+	private void splitImageToChannel(String title, int channel, int imgWidth, int imgHeight, ImagePlus originalImage) {
+		ImagePlus splitedImg = IJ.createImage(title, "8-bit", imgWidth, imgHeight, 1);
+		ImageProcessor imgProcessor = splitedImg.getProcessor();
+
+		for (int row = 0; row < imgHeight; row++) {
+			for (int column = 0; column < imgWidth; column++) {
+				int pixelValueArray[] = originalImage.getPixel(column, row);
+				int pixelValue = pixelValueArray[channel];
+				imgProcessor.putPixel(column, row, pixelValue);
+			}
+		}
+
+		splitedImg.show();
+
+	}
+
+}
